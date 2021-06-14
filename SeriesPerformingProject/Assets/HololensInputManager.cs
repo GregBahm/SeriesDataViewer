4465a18d62d3cfdf5b2f96bd55d149ae79a9ec66
Greg Bahm <grbahm@microsoft.com>
2021-06-13 18:13:31 -0700

    Had to break this out into two different projects because I couldn't get the hand tracking to load in the ray tracing project (seems somehow related to DirectX 12...)
    Began work on the Holographic interaction for the SeriesPerforming Project

---
+++ SeriesPerformingProject/Assets/HololensInputManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HololensInputManager : MonoBehaviour
+{
+    public float Deadzone = .1f;
+
+    private Transform transformHelper;
+
+    public Transform DrillDownTip;
+    public float DrilldownThreshold;
+
+    public PinchDetector PinchDetector;
+
+    public Transform MainStage;
+
+    public enum LeftHandToolMode
+    {
+        Dragging,
+        Rotating,
+        Resizing
+    }
+
+    public LeftHandToolMode toolMode;
+
+    private void Start()
+    {
+        transformHelper = new GameObject("Transform Helper").transform;
+    }
+
+    void Update()
+    {
+        UpdatePinchAndDrag();
+        UpdateDrillDown();
+    }
+
+    private void UpdatePinchAndDrag()
+    {
+        switch (toolMode)
+        {
+            case LeftHandToolMode.Dragging:
+                UpdateDragging();
+                break;
+            case LeftHandToolMode.Rotating:
+                UpdateRotating();
+                break;
+            case LeftHandToolMode.Resizing:
+            default:
+                UpdateResizing();
+                break;
+        }
+    }
+
+    private bool wasResizing;
+    private void UpdateResizing()
+    {
+        throw new NotImplementedException();
+    }
+
+    private bool wasRotating;
+    private void UpdateRotating()
+    {
+        throw new NotImplementedException();
+    }
+
+    private Vector3 transformHelperStartPos;
+    private Vector3 mainStageStartPos;
+    private bool wasDragging;
+    private void UpdateDragging()
+    {
+        if(!wasDragging && PinchDetector.PinchBeginning)
+        {
+            transformHelper.position = PinchDetector.PinchPoint.position;
+            transformHelperStartPos = transformHelper.position;
+            mainStageStartPos = MainStage.position;
+        }
+        if(PinchDetector.Pinching)
+        {
+            transformHelper.position = GetDeadzoneMovement();
+            MainStage.position = mainStageStartPos + (transformHelper.position - transformHelperStartPos);
+        }
+        wasDragging = PinchDetector.PinchBeginning;
+    }
+
+    private void UpdateDrillDown()
+    {
+        EpisodeBehavior closestEpisode = GetClosestEpisode();
+        EpisodeDrillDownManager.Instance.DrilledEpisode = closestEpisode;
+    }
+    
+    private Vector3 GetDeadzoneMovement()
+    {
+        Vector3 toTarget = PinchDetector.PinchPoint.position - transformHelper.position;
+        float distToTarget = toTarget.magnitude;
+        float deadDist = Mathf.Max(0, distToTarget - Deadzone);
+        return transformHelper.position + toTarget.normalized * deadDist;
+    }
+
+    private EpisodeBehavior GetClosestEpisode()
+    {
+        ShowBehavior activeShow = MainScript.Instance.ShownSeries;
+        float minDist = DrilldownThreshold;
+        EpisodeBehavior ret = null;
+        foreach(EpisodeBehavior episode in activeShow.EpisodeBehaviors)
+        {
+            BoxCollider collider = episode.GetComponent<BoxCollider>();
+            Vector3 boundsPoint = collider.ClosestPointOnBounds(DrillDownTip.position);
+            float dist = (DrillDownTip.position - boundsPoint).magnitude;
+            if(dist < minDist)
+            {
+                ret = episode;
+                minDist = dist;
+            }
+        }
+        return ret;
+    }
+}
---
+++ SeriesPerformingProject/Assets/SeriesDataViewer/Scripts/EpisodeDrillDownManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class EpisodeDrillDownManager : MonoBehaviour
+{
+    public static EpisodeDrillDownManager Instance { get; private set; }
+    public EpisodeBehavior DrilledEpisode { get; set; }
+
+    public CanvasGroup AlphaElement;
+    public TextMeshProUGUI Title;
+    public TextMeshProUGUI SeasonEpisode;
+    public TextMeshProUGUI ImdbRating;
+    public TextMeshProUGUI Nelson;
+
+    public Color InactiveTitleColor;
+    public Color ActiveTitleColor;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        UpdateAlphaElement();
+        if(DrilledEpisode != null)
+        {
+            Title.text = DrilledEpisode.Data.Title;
+            SeasonEpisode.text = "Season " + DrilledEpisode.Data.Season + " episode " + DrilledEpisode.Data.Episode;
+            ImdbRating.text = DrilledEpisode.Data.ImdbRating.ToString();
+            Nelson.text = DrilledEpisode.Data.NealsonRating.ToString();
+        }
+        //UpdateDepthOfField();
+    }
+
+    private void UpdateAlphaElement()
+    {
+        float alphaTarget = DrilledEpisode != null ? 1f : 0;
+        AlphaElement.alpha = Mathf.Lerp(alphaTarget, AlphaElement.alpha, Time.deltaTime * 30);
+    }
+}
